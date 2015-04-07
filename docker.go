01150b924cddd6f1d3c6dd21e5917aa6365042e8
Michael Boudreau <mike@cardence.com>
2015-04-07 12:14:40 -0700

    Fix Docker provider

--- docker.go
+++ docker.go
@@ -2,34 +2,59 @@ package env
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
+// DockerProvider is an implementation of a EnvReader
 type DockerProvider struct{}
 
+// Getenv looks for docker specific environment variables and returns
+// either the value or empty string, if nothing is found.
 func (er *DockerProvider) Getenv(key string) string {
-	return "docker unimplemented"
+	dockerEnv := extractDockerEnvironment()
+	return dockerEnv[key]
 }
 
-func translateEnvsToExpected(key string) ([]string, error) {
-	envs := make([]string, 10)
-	for _, val := range os.Environ() {
-		switch {
-		case strings.HasSuffix(val, "_PORT"):
-			envs = append(envs, fmt.Sprintf("%vURL", strings.TrimSuffix(val, "_PORT")))
-		case strings.Contains(val, "_PORT_"):
-		case strings.Contains(val, "_ENV_"):
-			vars := strings.Split(val, "_ENV_")
-			if len(vars) != 2 {
-				return nil, fmt.Errorf("length of %v is not equal to 2 after split on _ENV_", val)
-			}
-			envs = append(envs, fmt.Sprintf("%v_%v", vars[0], vars[1]))
+func extractDockerEnvironment() map[string]string {
+	normalizedEnvironment := make(map[string]string)
 
+	for _, entry := range os.Environ() {
+		splitEntry := strings.SplitN(entry, "=", 2)
+		key := splitEntry[0]
+		val := splitEntry[1]
+
+		switch {
+		case strings.HasSuffix(key, "_PORT") && !strings.Contains(key, "_PORT_"):
+			url := fmt.Sprintf("%v_URL", strings.TrimSuffix(key, "_PORT"))
+			normalizedEnvironment[url] = val
+		case strings.Contains(key, "_PORT_"):
+			vars := strings.Split(key, "_PORT_")
+			switch {
+			case strings.Contains(key, "_PROTO"):
+				proto := fmt.Sprintf("%v_PROTO", vars[0])
+				normalizedEnvironment[proto] = val
+			case strings.Contains(key, "_ADDR"):
+				addr := fmt.Sprintf("%v_ADDR", vars[0])
+				host := fmt.Sprintf("%v_HOST", vars[0])
+				normalizedEnvironment[addr] = val
+				normalizedEnvironment[host] = val
+			case strings.Contains(key, "_PORT"):
+				port := fmt.Sprintf("%v_PORT", vars[0])
+				normalizedEnvironment[port] = val
+			}
+		case strings.Contains(key, "_ENV_"):
+			vars := strings.Split(key, "_ENV_")
+			if len(vars) != 2 {
+				log.Printf("length of %v is not equal to 2 after split on _ENV_", key)
+			}
+			envKey := fmt.Sprintf("%v_%v", vars[0], vars[1])
+			normalizedEnvironment[envKey] = val
 		}
 	}
 
-	return envs, nil
+	return normalizedEnvironment
 }
 
 /*
