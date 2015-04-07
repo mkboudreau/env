71a256d9ca5a2ba1881d769ee1052b9dc73dc65a
Michael Boudreau <mike@cardence.com>
2015-04-07 12:14:18 -0700

    Make tests more fine grained and better logging

--- docker_test.go
+++ docker_test.go
@@ -18,8 +18,76 @@ var dockerTestCases = []struct {
 		"DB_PORT_5555_TCP_PORT":  "5555",
 		"DB_ENV_ANYTHING":        "TEST123",
 	},
-		[]string{"", "DB_PORT", "DB_HOST", "DB_ADDR", "DB_URL", "DB_ANYTHING"},
-		[]string{"", "5555", "24.50.222.1", "24.50.222.1", "tcp://24.50.222.1:5555", "TEST123"},
+		[]string{""},
+		[]string{""},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{"DB_PORT"},
+		[]string{"5555"},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{"DB_HOST"},
+		[]string{"24.50.222.1"},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{"DB_ADDR"},
+		[]string{"24.50.222.1"},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{"DB_URL"},
+		[]string{"tcp://24.50.222.1:5555"},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{"DB_ANYTHING"},
+		[]string{"TEST123"},
+	},
+	{map[string]string{
+		"DB_NAME":                "/somename/db",
+		"DB_PORT":                "tcp://24.50.222.1:5555",
+		"DB_PORT_5555_TCP_PROTO": "tcp",
+		"DB_PORT_5555_TCP_ADDR":  "24.50.222.1",
+		"DB_PORT_5555_TCP_PORT":  "5555",
+		"DB_ENV_ANYTHING":        "TEST123",
+	},
+		[]string{},
+		[]string{},
+		//[]string{"", "DB_PORT", "DB_HOST", "DB_ADDR", "DB_URL", "DB_ANYTHING"},
+		//[]string{"", "5555", "24.50.222.1", "24.50.222.1", "tcp://24.50.222.1:5555", "TEST123"},
 	},
 	{map[string]string{
 		"DB_NAME":                "/somename/db",
@@ -41,7 +109,7 @@ func setupEnvironmentWithMap(envMap map[string]string) {
 }
 
 func clearEnvironmentWithMap(envMap map[string]string) {
-	for key, _ := range envMap {
+	for key := range envMap {
 		os.Unsetenv(key)
 	}
 }
@@ -53,7 +121,9 @@ func TestDockerProviderTaskRunner(t *testing.T) {
 		setupEnvironmentWithMap(testCase.setup)
 
 		if IsNotEqual(provider, testCase.test, testCase.expected) {
-			t.Errorf("actual %v does not equal expected %v", testCase.test, testCase.expected)
+			t.Errorf("Failure: actual %v does not equal expected %v", testCase.test, testCase.expected)
+		} else {
+			t.Logf("Success: actual %v does equal expected %v", testCase.test, testCase.expected)
 		}
 
 		clearEnvironmentWithMap(testCase.setup)
@@ -68,7 +138,7 @@ func IsEqual(reader EnvReader, keys []string, test []string) bool {
 		return false
 	}
 
-	for i, _ := range keys {
+	for i := range keys {
 		if reader.Getenv(keys[i]) != test[i] {
 			return false
 		}
